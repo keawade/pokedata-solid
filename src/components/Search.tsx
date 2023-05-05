39b37ada1e71f5b29fffe89a2b4c364ef3af8ff9
Keith Wade <keawade@protonmail.com>
2023-05-05 08:43:10 -0400

    Yolo broken select attempt

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,7 +1,8 @@
 import { Component } from 'solid-js';
 import { default as pokemon } from '../data/pokemon.json';
 import { useHistory } from '../HistoryProvider';
-import { Select, createOptions } from '@thisbeyond/solid-select';
+import { createOptions } from '@thisbeyond/solid-select';
+import { Select } from './Select';
 import { Pokemon } from '../interfaces/Pokemon';
 
 export const Search: Component = () => {
@@ -9,6 +10,7 @@ export const Search: Component = () => {
   const props = createOptions(pokemon, { key: 'name' });
   return (
     <Select
+      placeholder="Select a Pokemon!"
       {...props}
       onChange={(item: Pokemon) => {
         selectPokemon(item.id);
